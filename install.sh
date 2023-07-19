2da463302a30515160e5f4c97f97d4277a2745d0
fzscript <139060278+fzscript@users.noreply.github.com>
2023-07-19 12:09:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ servers=("google.com" "raw.githubusercontent.com")
 
 for server in "${servers[@]}"; do
     if ! ping -c 1 -W 3 "$server"&> /dev/null; then
-        echo -e "\e[1;31m$server tidak dapat dijangkau dengan jaringan Anda saat ini.\nUbah konfigurasi jaringan Anda.\e[0m"
+        echo -e "\e[1;31m$server tidak dapat dijangkau dengan jaringan Anda saat ini.\e[0m\nUbah konfigurasi jaringan Anda."
     fi
 done
 
 if [ -z "$TERMUX_VERSION" ]; then
-    echo -e "\e[1;31mTermux tidak terdeteksi!\e[0m\n\e[1;31mInstal dibatalkan.\e[0m"
+    echo -e "\e[0;31mTermux tidak terdeteksi!\e[0m\nInstal dibatalkan."
     exit 1
 fi
 
@@ -20,11 +20,11 @@ fi
 
 if ! command -v git &> /dev/null; then
     if ! pkg update -y -o Dpkg::Options::="--force-confnew"; then
-        echo -e "\e[1;31mUps!
+        echo -e "\e[0;33mUps!
 Kemungkinan penyebab kesalahan:
 1. Termux dari Play Store tidak terawat, Unduh Termux dari GitHub.
 2. Koneksi Internet tidak stabil.
-3. Repositori bermasalah, Hapus Data Termux dan Coba lagi."
+3. Repositori bermasalah, Hapus Data Termux dan Coba lagi.\e[0m"
         exit 1
     fi
     pkg install git -y -o Dpkg::Options::="--force-confnew"
@@ -33,7 +33,7 @@ fi
 if git clone --depth=1 https://github.com/fzscript/Script-II.git; then
     ./Script-II/fz
 else
-    echo -e "\e[1;31mInstal Gagal!\e[0m"
+    echo -e "\e[0;31mInstal Gagal!\e[0m"
     echo "Harap Coba lagi"
     exit 1
 fi
